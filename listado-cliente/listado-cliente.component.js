7fb66bbded28f48f3bc6c2d3b3bac6b9a80e2947
Andrés Mata <andres_mata21@outlook.com>
2022-06-13 14:17:59 -0500

    Pantalla de carga y prueba de height

--- listado-cliente/listado-cliente.component.js
+++ listado-cliente/listado-cliente.component.js
@@ -4,8 +4,11 @@ angular
         templateUrl: 'listado-cliente/listado-cliente.template.html',
         controller: ['Ventas', function ListadoClienteController(Ventas) {
             let self = this;
+            self.loading = true;
+
             self.ventas = Ventas.getVentas(({ clientes }) => {
                 this.ventas = clientes;
+                self.loading = false;
             });
 
             // Referenicas HTML
